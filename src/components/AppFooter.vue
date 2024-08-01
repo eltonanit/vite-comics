2f5c7a09cf1aeefb52cde18d07ff5c99d3439bd4
eltonanit <elton.ani@gmail.com>
2024-08-01 22:31:04 +0200

    [UPD] Added AppFooter.vue

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <footer>
+<div class="footer-links">
+
+       
+    </div>
+    <div lass="footer-menu">
+
+       
+    </div>
+       
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+    
+       
+        .footer-links {
+            background-color: #0082F9;
+            color: #fff;
+            padding: 20px 0;
+        }
+        .footer-menu {
+            background-color: #222;
+            color: #fff;
+            padding: 20px 0;
+        }
+       
+</style>
