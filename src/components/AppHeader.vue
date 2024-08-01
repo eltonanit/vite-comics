748c373fcdd71e442329035483cd5ab1e82664b0
eltonanit <elton.ani@gmail.com>
2024-08-01 20:08:20 +0200

    [UPD] Added dc-logo and header menu in AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,44 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-2">
+                    <div class="text-center">
+                        <img src="../assets/dc-logo.png" /> 
+                    </div>
+                </div>
+                <div class="col-9"> 
+                    <ul class="list-unstyled d-flex m-2">
+                        <li>CHARACTERS</li>
+                        <li>COMICS</li>
+                        <li>MOVIES</li>
+                        <li>TV</li>
+                        <li>GAMES</li>
+                        <li>COLLECTIBLES</li>
+                        <li>VIDEOS</li>
+                        <li>FANS</li>
+                        <li>NEWS</li>
+                        <li>SHOP</li>
+                        
+                    </ul>
+
+                </div>
+            </div>
+        </div>                   
+               
+             
+                
+                 
+
+            
+    </header>
+</template>
+
 <script>
 export default {
     
 }
-</script><template lang="">
-    <div>
-        
-    </div>
-</template>
+</script>
 
 <style lang="">
     
