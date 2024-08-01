87443ae43afdf051c6ee868ed591ac8f5dd3a1be
eltonanit <elton.ani@gmail.com>
2024-08-02 00:37:00 +0200

    [UPD] Added footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,14 +5,86 @@ export default {
 </script>
 <template lang="">
     <footer>
-<div class="footer-links">
-
-       
+  <div class="footer-links">
+      <div class="container">
+            <div class="row">
+                <div class="col-2">
+                    <img src="../img/buy-comics-digital-comics.png"height="40" />DIGITAL COMICS</div>
+                <div class="col-2"><img src="../img/buy-comics-merchandise.png"height="40" />DC MERCHANDISE</div>
+                <div class="col-2"><img src="../img/buy-comics-subscriptions.png"height="40" />SUBSCRIPTION</div>
+                <div class="col-2"><img src="../img/buy-comics-shop-locator.png"height="40" />COMIC SHOP LOCATOR</div>
+                <div class="col-2">
+                    <img src="../img/buy-dc-power-visa.svg"height="40" />DC POWER VISA</div>
+            </div>
+        </div>
     </div>
-    <div lass="footer-menu">
+    
+     
+    
 
-       
+     
+
+
+ 
+    
+     <div class="footer-menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <h5>DC COMICS</h5>
+                    <ul class="list-unstyled">
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <h5>SHOP</h5>
+                    <ul class="list-unstyled">
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>DC</h5>
+                    <ul class="list-unstyled">
+                        <li>Terms Of Use</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>SITES</h5>
+                    <ul class="list-unstyled">
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li> 
+     
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <img src="../img/dc-logo-bg.png"  class="img-fluid">
+                     
+                </div>
+            </div>
+        </div>
     </div>
+
+    
+
+ 
        
     </footer>
 </template>
@@ -24,9 +96,16 @@ export default {
             background-color: #0082F9;
             color: #fff;
             padding: 20px 0;
+            font-weight: 40;
+            font-size: 10px;
+        }
+        .footer-links  img{
+            
+            font-size: 20px;
         }
         .footer-menu {
-            background-color: #222;
+            background-image: url('../img/footer-bg.jpg');
+            background-size: cover;
             color: #fff;
             padding: 20px 0;
         }
