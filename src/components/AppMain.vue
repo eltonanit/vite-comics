fa9c5ef58b39f5e4c63b37aafcc5e5822c4c6368
eltonanit <elton.ani@gmail.com>
2024-08-01 22:31:28 +0200

    [UPD] Added AppMain.vue

---
+++ src/components/AppMain.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <main>
+
+         <div class="content-area">
+        <div class="container">
+            <p> -> Content goes here <- </p>
+        </div>
+    </div>
+
+
+
+       
+    </main>
+</template>
+
+<style lang="scss" scoped>
+    
+      .content-area {
+            background-color: #222;
+            color: #fff;
+             font-weight: 600;
+            padding: 20px;
+        }
+       
+
+</style>
